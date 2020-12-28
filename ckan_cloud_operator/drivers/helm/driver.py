c7ad4c253c2a8c6c7b72816faae8e281c652129c
ismailshahzad <ismailshahzadmirza@gmail.com>
2020-12-28 23:25:13 +0500

    Update

--- ckan_cloud_operator/drivers/helm/driver.py
+++ ckan_cloud_operator/drivers/helm/driver.py
@@ -40,6 +40,7 @@ def init(tiller_namespace_name):
     kubectl.apply(cluster_role_binding)
     subprocess.check_call(
         f"helm init --upgrade --service-account {tiller_namespace_name}-tiller --tiller-namespace {tiller_namespace_name} --history-max 10 --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@'",
+        f'helm init --stable-repo-url https://charts.helm.sh/stable --service-account tiller',
         shell=True
     )
 
