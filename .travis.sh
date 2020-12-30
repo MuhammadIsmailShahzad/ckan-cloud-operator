ed6d19732d4c69d7cd030b76e0427c9cdec74ca3
ismailshahzad <ismailshahzadmirza@gmail.com>
2020-12-30 15:35:02 +0500

    Update

--- .travis.sh
+++ .travis.sh
@@ -65,7 +65,7 @@ elif [ "${1}" == "install-tools" ]; then
       echo Azure CLI Installed Successfully!
    fi
 
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.9/bin/linux/amd64/kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.12/bin/linux/amd64/kubectl
     chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
     kubectl version --client
     echo Kubectl Installed Successfully!
